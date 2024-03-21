f108f177804791e9efda19ef6970e2da5cdba282
carloscrw <carlos.c.r.w@gmail.com>
2024-03-21 15:29:19 -0300

    teste100

--- js/script.js
+++ js/script.js
@@ -1,28 +1,47 @@
 document.addEventListener("DOMContentLoaded", function () {
-  var instagramLink = document.querySelector(".instagram");
-  var whatsappLink = document.querySelector(".whatsapp");
+  var liberarInternetBtn = document.getElementById("liberarInternet");
 
-  // Monitorando cliques nos links do Instagram e WhatsApp
-  instagramLink.addEventListener("click", function (event) {
-    event.preventDefault(); // Evitar o comportamento padrão do link
-    liberarAcessoInternet();
-  });
-
-  whatsappLink.addEventListener("click", function (event) {
-    event.preventDefault(); // Evitar o comportamento padrão do link
+  liberarInternetBtn.addEventListener("click", function () {
     liberarAcessoInternet();
   });
 
   function liberarAcessoInternet() {
-    var user_hash = getUserHash(); // Função para obter o user_hash (pode ser gerado aleatoriamente)
+    var continueUrl = "http://www.intelbras.com.br"; // URL que o usuário tentou acessar
+    var apIp = "10.0.0.1"; // Endereço IP do Access Point
+    var apMac = "00:11:22:33:44:66"; // Endereço MAC do Access Point
+    var clientMac = "00:11:22:33:44:55"; // Endereço MAC do cliente Wireless
+    var radio = "radio0"; // Nome do rádio Wireless
+    var ssid = "MY_CAPTIVE"; // Nome do SSID Wireless
     var ts = Math.floor(Date.now() / 1000); // Timestamp atual em segundos
-    var redirect_uri = "http://10.0.0.1:2061/cp/itbcaptive.cgi"; // URL de redirecionamento conforme a documentação
-    var final_url = redirect_uri + "?ts=" + ts + "&user_hash=" + user_hash; // Construir a URL final
+    var redirectUri = "http://10.0.0.1:2061/cp/itbcaptive.cgi"; // URL de redirecionamento
+    var userHash = generateUserHash(); // Gerar user_hash aleatório
 
-    window.location.href = final_url;
+    var finalUrl =
+      redirectUri +
+      "?" +
+      "continue=" +
+      encodeURIComponent(continueUrl) +
+      "&ip=" +
+      apIp +
+      "&ap_mac=" +
+      apMac +
+      "&mac=" +
+      clientMac +
+      "&radio=" +
+      radio +
+      "&ssid=" +
+      ssid +
+      "&ts=" +
+      ts +
+      "&redirect_uri=" +
+      encodeURIComponent(redirectUri) +
+      "&user_hash=" +
+      userHash;
+
+    window.location.href = finalUrl;
   }
 
-  function getUserHash() {
+  function generateUserHash() {
     // Função para gerar um user_hash aleatório
     return Math.random().toString(36).substring(2);
   }
