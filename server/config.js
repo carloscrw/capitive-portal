dbf72577479a9685b1bd770cd5aa7eb611c7cae7
carloscrw <carlos.c.r.w@gmail.com>
2024-03-22 14:49:29 -0300

    teste 10000000

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-
+console.log("Valor de SECRET_KEY:", process.env.SECRET_KEY);
 module.exports = {
   serverIP: process.env.SERVER_IP || "127.0.0.1",
   serverPort: process.env.SERVER_PORT || 3333,
