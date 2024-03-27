a34a73099aa52793e9512ca520f0819a9756a3b5
carloscrw <carlos.c.r.w@gmail.com>
2024-03-27 10:21:16 -0300

    img

--- server/config.js
+++
@@ -1,6 +0,0 @@
-require("dotenv").config();
-console.log("Valor de SECRET_KEY:", process.env.SECRET_KEY);
-module.exports = {
-  serverIP: process.env.SERVER_IP || "127.0.0.1",
-  serverPort: process.env.SERVER_PORT || 3333,
-};
